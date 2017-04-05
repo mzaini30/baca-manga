4ce8251ec7b99ddcce1904f50273b1373abe67f2
mzaini30 <muhzaini30@gmail.com>
2017-04-05 22:56:35 +0800

    sip

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
-import glob
+import glob, re, os
 
 lokasi = "/sdcard/Download/baca manga/"
-folder = raw_input("Masukkan nama folder: ")
+buatfile = []
+filehtml = []
+nyarigambar = []
+css = """<style>
+img {
+	display: none;
+}
+img[src*=jpg],
+img[src*=png],
+img[src*=gif]{
+	display: block;
+	width: 100%;
+}
+</style>"""
+    
+html = glob.glob(lokasi+"*.html")
+for x in html:
+    os.remove(x)
 
-data = glob.glob(lokasi+folder+"/*.jpg")
-hasil = open(lokasi+"output.html", "w")
+folder = glob.glob(lokasi+"*/")
 
-hasil.write("<title>"+folder+"</title>")
-hasil.write("<body onclick='document.body.webkitRequestFullScreen()' style='position: fixed; width: 100%; height: 100%; overflow: auto;'>")
-hasil.write("<center>")
-for x in data:
-    hasil.write("<img src='"+x+"' width='100%'/>")
-hasil.write("</center>")
-hasil.write("</body>")
+for x in folder:
+    x = re.sub(r""+lokasi, r"hdhsbbshs", x)
+    x = re.sub(r"/", r".html", x)
+    x = re.sub(r"hdhsbbshs", r""+lokasi, x)
+    buatfile.append(x)
+
+for x in buatfile:
+    filehtml.append(open(x, "w"))
+    
+for n, x in enumerate(folder):
+    nyarigambar.append(glob.glob(x+"*"))
+    for j, q in enumerate(nyarigambar[n]):
+        nyarigambar[n][j] = re.sub(r""+lokasi, r"", q)
+    
+for n, x in enumerate(filehtml):
+    x.write(css)
+    x.write("<body onclick='document.body.webkitRequestFullScreen()' style='position: fixed; width: 100%; height: 100%; overflow: auto;'>")
+    x.write("<center>")
+    for q in nyarigambar[n]:
+        x.write("<img src='"+q+"'/>")
+    x.write("</center>")
+    x.write("</body>")
